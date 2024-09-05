fa72a068424e0b724784f9d31491f3d77bc19974
pv <test@pv.com>
2024-09-05 11:23:42 +0200

    Ajout du fichier Mission.cpp et Mission.h

---
+++ Mission.h
@@ -0,0 +1,12 @@
+/*****************************************************************//**
+ * \file   Mission.h
+ * \brief  Declaration de la classe misssion
+ *
+ * \author Pier-Andre
+ * \date   September 2024
+ *********************************************************************/
+
+#ifndef MISSION_H
+#define MISSION_H
+#include <iostream>
+#include <string>
