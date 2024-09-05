2bac72d53f75bde522ca7084e3b2dc9561d3a267
pv <test@pv.com>
2024-09-05 12:02:11 +0200

    modification du fichier Mission.h avec la creation de la class Mission

--- Mission.h
+++ Mission.h
@@ -10,3 +10,54 @@
 #define MISSION_H
 #include <iostream>
 #include <string>
+
+
+ /*! \class Mission
+    * \brief classe representant une mission. 2 attributs prives
+    *
+    * Cette classe a une finalite pedagogique :
+    * \li : Rappel du C++
+    * \li : commentaire avec Doxygen
+    * \li : git, github
+    */
+class Mission
+{
+
+private:
+    /**
+     * Membre prive nbPersonne : contient le nombre de personne pour la mission
+     */
+    std::string nbPersonne;
+    /**
+     * Membre prive descriptifMission : contient le descriptif de la mission
+     */
+    std::string descriptifMission;
+    /**
+     * Membre prive nbHeure : contient le nombre d'heure de la mission
+    */
+    std::string nbHeure;
+
+
+
+protected:
+
+
+
+public:
+    /*Classe sous forme canonique*/
+    Mission();
+    ~Mission();
+    Mission(const Mission& entre);
+    Mission &operator = (const Mission& entre);
+
+    Mission(std::string nb, std::string ville);
+    std::string getNbPersonne();
+    void setNbPersonne(std::string nouveauNombrePersonne);
+    std::string getDescriptifMission();
+    void setDescriptifMission(std::string nouveauDescriptifMission);
+    std::string getNbHeure();
+    void setNbHeure(std::string nouveauNombreHeure);
+
+};
+
+#endif
